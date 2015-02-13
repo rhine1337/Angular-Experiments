75d5e09b7710d644f221cbb7e5c492742ec0b88e
rhine1337 <banerjee.debrup@gmail.com>
2015-02-13 10:18:33 +0530

    2

---
+++ public_html/app/app.modules.js
@@ -0,0 +1,5 @@
+// create the module and name it employeeApp
+// also include ngRoute for all our routing needs
+var employeeApp = angular.module('employeeApp', ['ngRoute']);
+var toolbarApp = angular.module('toolbarApp', []);
+var formProcessingApp = angular.module('formProcessingApp', []);
--- public_html/app/app.routes.js
+++ public_html/app/app.routes.js
@@ -1,38 +1,47 @@
-// script.js
+// configure our routes
+employeeApp.config(function($routeProvider) {
+    $routeProvider
+        // route for the home page
+        .when('/home', {
+            templateUrl : 'app/components/employeeForm/views/employeeForm.view.html',
+            controller  : 'empRegFormController'
+        })
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
+        })
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        })
+        .otherwise({redirectTo : '/home'});
+});
 
-    // create the module and name it employeeApp
-        // also include ngRoute for all our routing needs
-    var employeeApp = angular.module('employeeApp', ['ngRoute']);
-    // configure our routes
-    employeeApp.config(function($routeProvider) {
-        $routeProvider
-            // route for the home page
-            .when('/', {
-                templateUrl : 'app/components/employeeForm/views/employeeForm.view.html',
-                controller  : 'empRegFormController'
-            })
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            });
-    });
+// create the controller and inject Angular's $scope
+employeeApp.controller('empRegFormController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Employee Registration Form';
+});
 
-    // create the controller and inject Angular's $scope
-    employeeApp.controller('empRegFormController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Employee Registration Form';
-    });
+employeeApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
 
-    employeeApp.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
-    });
+employeeApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
 
-    employeeApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
-    });
+toolbarApp.controller('toolbarController', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };        
+}]);
+
+formProcessingApp.controller('regFormController',function($scope){
+    $scope.formData = {};
+    $scope.submission = false;
+    console.log($scope.formData);
+});
